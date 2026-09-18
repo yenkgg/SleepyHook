2f647af42e742ff8de18b9a2a3b524fd41dbad25
0es2 <contactyenkgg@gmail.com>
2026-09-18 20:40:01 +0300

    Update script URLs for RIVALS and ARSENAL

--- loader.lua
+++ loader.lua
@@ -11,9 +11,9 @@ end;
 
 if (game.GameId == 6035872082) then
     -- RIVALS
-	loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/626ac4a18ba4537e724ceefb06612bacd7d715986997678b7aa5a50d5b8dd3ef.lua"))();
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/yenkgg/SleepyHook/refs/heads/main/RIVALS.lua"))();
 
 elseif (game.GameId == 111958650 or game.PlaceId == 286090429) then
     -- ARSENAL
-	loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/9cc468003046ae1318e5891e11f6cc29c9d43d4a9aa19462fb2ccab5a4acb1c7.lua"))();
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/yenkgg/SleepyHook/refs/heads/main/ARSENAL.lua"))();
 end;
